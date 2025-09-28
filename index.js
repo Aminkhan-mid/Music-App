936b8b2c4b1352dae77d5a462b187ec6f4690580
Amin Khan <154885880+Aminkhan-mid@users.noreply.github.com>
2025-09-28 20:56:13 +0530

    added basic song profile.

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 fetch(`https://itunes.apple.com/search?term=arctic+monkeys+i+wanna+be+yours&media=music&entity=song&limit=1`)
     .then(res => res.json())
     .then(data => {
-        console.log(data.results[0])
-    })
+        for(let songs of data.results){
+            console.log(songs)
+            let artist = songs.artistName
+            let songName = songs.trackName
+            let songPreview = songs.previewUrl
+            let artWork = songs.artworkUrl100
+            document.getElementById('song').innerHTML = `   
+            <div class="song-bg">
+            <img src="${artWork}" alt="zoro" class="song-img">
+            <p class="song-title">${songName}</p>
+            <p class="song-title">By: ${artist}</p>
+            
+            <audio controls>
+                <source src="${songPreview}" type="audio/mpeg">
+                </audio>
+                <button >
+                    <i class="fa-solid fa-circle-play"></i>
+                </button>
+            </div>`
+        }
+})
 
 
 
