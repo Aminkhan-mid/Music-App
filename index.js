c46226ceb47defbb780b77fe6fd7f449e7fdfd8d
Amin Khan <154885880+Aminkhan-mid@users.noreply.github.com>
2025-09-28 18:53:28 +0530

    commit

---
+++ index.js
@@ -0,0 +1,5 @@
+fetch(`https://itunes.apple.com/search?term=arctic+monkeys+i+wanna+be+yours&media=music&entity=song&limit=1`)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data.results[0])
+    })
